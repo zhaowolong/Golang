f9d6535493e8fa5d5718b1b4b884eb84dc5f5888
wolong zhao <512830036@qq.com>
2016-03-23 14:14:56 +0800

    update

--- http/httpunilighttest.go
+++ http/httpunilighttest.go
@@ -4,8 +4,11 @@ import (
 	"crypto/md5"
 	"fmt"
 	"strconv"
+//	"os"
+//	"os/exec"
 	//"io"
 	"io/ioutil"
+//	"path/filepath"
 	"time"
 	//"log"
 	//"math/rand"
@@ -15,25 +18,54 @@ import (
 	"git.code4.in/mobilegameserver/logging"
 )
 
-var loginUrl = "http://14.17.104.56:8000/httplogin"
-var gameid = 170
+//var loginUrl = "http://14.17.104.56:8000/zone/clientlog"
+var loginUrl = "http://server.login.bwgame.com.cn:7000/httplogin"
+var gameid = 300 
 var zoneid = 301
-
-var str = fmt.Sprintf(`{"do":"plat-token-login","gameid":170,"zoneid":301,"data":{"platinfo":{"account":"","platid":0,"email":"whj@whj.whj","gender":"male","nickname":"navy1125","timestamp":"12345","uid":"10722"}}}`)
+var gateway1 = 0
+var gateway2 = 0
+var c = make(chan int) 
+var shutdown = 0
+var cnn = 1 
+var send =  1
+var str = fmt.Sprintf(`{"do":"plat-token-login","gameid":300,"zoneid":301,"data":{"plataccount":"zwl", "platinfo":{"account":"zwl","platid":0,"email":"whj@whj.whj","gender":"male","nickname":"navy1125","timestamp":"12345","uid":"10722"}}}`)
 var countMsg = 0
+
+
 func main(){	
-	for i := 1; i < 10000; i++ {
+	/*
+	filePath, _ := filepath.Abs(os.Args[0])
+	if os.Getppid() != 1 {
+			logging.Info("server start as daemon:%s,%v", filePath, os.Args[1:])
+			cmd := exec.Command(filePath, os.Args[1:]...)
+			cmd.Start()
+			os.Exit(0)
+		}
+	*/
+
+
+	for i := 0; i < cnn; i++ {
 		goindex := i 
 		go connect(goindex)
+		logging.Info("go rountion %d", i)
+		time.Sleep(10000)
 	}
-	c := make(chan int, 1)
 	fmt.Println(<-c)
 }
 
 func connect(goindex int) {
-	// plat-token-login
-	plattokenlogin := fmt.Sprintf(`{"do":"plat-token-login", "gameid":170, "zoneid":301, "data":{"platinfo":{"account":"", "platid":0}}}`)
 	count := fmt.Sprintf("%s: %d", "plattokenlogin", goindex)
+	// get serverlist 
+	serverlist := fmt.Sprintf(`{"do":"request-zone-list", "gameid":%d, "zoneid":301, "data":{"platinfo":{"account":"", "platid":0}}}`, gameid)
+	bOk, zonelist := httpsend(loginUrl,serverlist, count)
+	if !bOk {
+		logging.Error("httpsend error plat-token-login ")
+		return 
+	}
+
+	logging.Info("zonelist  %s", string(zonelist))
+	// plat-token-login
+	plattokenlogin := fmt.Sprintf(`{"do":"plat-token-login", "gameid":%d, "zoneid":301, "data":{"platinfo":{"account":"zwl", "platid":0}}}`, gameid)
 	bOk, token := httpsend(loginUrl, plattokenlogin, count)
 	if !bOk {
 		logging.Error("httpsend error plat-token-login ")
@@ -47,9 +79,19 @@ func connect(goindex int) {
 	unigame_plat_key := js.Get("unigame_plat_key").MustString()
 	unigame_plat_login := js.Get("unigame_plat_login").MustString()
 	uid := js.Get("data").Get("uid").MustString()
-	// select-zone
+	// get userzoneinfo
 	data := "{}"
-	signurl, dataSend := sendSign(uid, "request-select-zone", data, unigame_plat_key, unigame_plat_login, loginUrl, 170, 301)
+	signurl, dataSend := sendSign(uid, "request-user-zone-info", data, unigame_plat_key, unigame_plat_login, loginUrl, gameid, zoneid)
+	bOk, ret := httpsend(signurl, string(dataSend), count)
+	if bOk != true {
+		logging.Error("httpsend error request-user-zone-info error")
+		return 
+	}
+
+	logging.Info("userzoneinfo  %s", string(ret))
+	// select-zone
+	data = "{}"
+	signurl, dataSend = sendSign(uid, "request-select-zone", data, unigame_plat_key, unigame_plat_login, loginUrl, gameid, zoneid)
 	bOk, token = httpsend(signurl, string(dataSend), count)
 	if !bOk {
 		logging.Error("httpsend error select-zone error")
@@ -61,12 +103,23 @@ func connect(goindex int) {
 		return 
 	}
 	gatewayurl := js.Get("data").Get("gatewayurl").MustString()
+	accountid := js.Get("data").Get("zoneuid").MustString()
+	uid = fmt.Sprintf("%s", accountid)
+	logging.Info("玩家分配的区的uid是%d, %s", accountid, string(token))
+	logging.Info("gatewayurl %s", gatewayurl)
+	if gatewayurl == "http://14.17.104.56:6502/shen/user/http" {
+		gateway2 += 1
+	} else{ 
+		gateway1 += 1
+	}	
+	logging.Info("gateway1 %d, gateway2 %d", gateway1, gateway2)
 	// sendTounilight
-	for j:=0; j<1000; j +=1 {
-		signurl, dataSend := sendSign(uid, "Cmd.UserInfoSynRequestLbyCmd_C", "{}", unigame_plat_key, unigame_plat_login, gatewayurl, 170, 301)
+	for j:=0; j<send; j +=1 {
+		signurl, dataSend := sendSign(uid, "Cmd.Login_C", "{}", unigame_plat_key, unigame_plat_login, gatewayurl, gameid, zoneid)
 		bOk, token = httpsend(signurl, string(dataSend), count)
+		shutdown += 1
 		if !bOk {
-			logging.Error("httpsend error UserInfoSynRequestLbyCmd_c")
+			logging.Error("httpsend errordquestLbyCmd_c")
 			return 
 		}
 		js, err = sjson.NewJson(token)
@@ -74,9 +127,14 @@ func connect(goindex int) {
 			logging.Error("UserInfoSynRequestLbyCmd_C zone to json error")
 			return 
 		}
-		desc := js.Get("data").Get("desc").MustString()
+		js.Get("data").Get("desc").MustString()
 		countMsg += 1
-		logging.Info("rev unilight%s, 第%d个携程中的第%d次访问， 共访问次数%d", desc, goindex, j, countMsg)
+		//logging.Info("rev unilight%s, 第%d个携程中的第%d次访问， 共访问次数%d", desc, goindex, j, countMsg)
+	}
+
+	
+	if shutdown > send*cnn-10000 {
+		c <- 1	
 	}
 }
 
---
+++ http/websockettest.go
@@ -0,0 +1,27 @@
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+//	"os"
+//	"os/exec"
+	//"io"
+	"io/ioutil"
+//	"path/filepath"
+	"time"
+	//"log"
+	//"math/rand"
+	"net/http"
+	"strings"
+	sjson "github.com/bitly/go-simplejson"
+	"git.code4.in/mobilegameserver/logging"
+)
+
+func requestWebsocketUrl(){
+
+}
+
+func connectWebSocket(url string){
+
+}
+
