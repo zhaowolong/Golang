ba2c3e95421496b8bb6c8777d2abbd0247206573
wanghaijun <wanghaijun@ztgame.com>
2016-05-16 19:19:49 +0800

    连接第三方数据

--- http/3rdpartypltserver/httpserver.go
+++ http/3rdpartypltserver/httpserver.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+var debug = flag.Bool("debug", true, "enable debugging")
+var password = flag.String("password", "asd123", "the database password")
+var port *int = flag.Int("port", 1433, "the database port")
+var server = flag.String("server", "222.186.52.217", "the database server")
+var user = flag.String("user", "sa", "the database user")
 
 func login(rw http.ResponseWriter, req *http.Request) {
 	log.Println("收到了login ip", string(req.RemoteAddr))
@@ -37,6 +46,42 @@ func pay(rw http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	flag.Parse() // parse the command line args
+
+	if *debug {
+		fmt.Printf(" password:%s\n", *password)
+		fmt.Printf(" port:%d\n", *port)
+		fmt.Printf(" server:%s\n", *server)
+		fmt.Printf(" user:%s\n", *user)
+	}
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", *server, *user, *password, *port)
+	if *debug {
+		fmt.Printf(" connString:%s\n", connString)
+	}
+	conn, err := sql.Open("mssql", connString)
+	if err != nil {
+		log.Fatal("Open connection failed:", err.Error())
+	}
+	defer conn.Close()
+
+	stmt, err := conn.Prepare("select 1, 'abc'")
+	if err != nil {
+		log.Fatal("Prepare failed:", err.Error())
+	}
+	defer stmt.Close()
+
+	row := stmt.QueryRow()
+	var somenumber int64
+	var somechars string
+	err = row.Scan(&somenumber, &somechars)
+	if err != nil {
+		log.Fatal("Scan failed:", err.Error())
+	}
+	fmt.Printf("somenumber:%d\n", somenumber)
+	fmt.Printf("somechars:%s\n", somechars)
+
+	fmt.Printf("bye\n")
+
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/pay", pay)
 	log.Fatal(http.ListenAndServe("127.0.0.1:8126", nil))
